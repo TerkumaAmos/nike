8aac0e11d3277ed307b7c2cd4998d3d0e113a910
TerkumaAmos <agberterkuma2020@gmail.com>
2024-12-06 16:08:57 +0100

    new

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -1,15 +1,15 @@
 final products = [
   {
     'id': '6',
-    'title': 'Men\'s Nike Shoes',
+    'title': 'Nike Shoes',
     'price': 140.52,
-    'imageURL': 'assets/images/shoes 6.png',
+    'imageURL': 'assets/images/shoe 8.jpeg',
     'company': 'Nike',
     'sizes': [8, 9, 10, 11],
   },
   {
     'id': '6',
-    'title': 'Men\'s Nike Shoes',
+    'title': 'Puma Shoes',
     'price': 190.52,
     'imageURL': 'assets/images/shoes 6.png',
     'company': 'Nike',
@@ -17,15 +17,15 @@ final products = [
   },
   {
     'id': '6',
-    'title': 'Men\'s Nike Shoes',
+    'title': 'Adidas Shoes',
     'price': 70.52,
-    'imageURL': 'assets/images/shoes 6.png',
+    'imageURL': 'assets/images/shoe 7.jpeg',
     'company': 'Nike',
     'sizes': [8, 9, 10, 11],
   },
   {
     'id': '6',
-    'title': 'Men\'s Nike Shoes',
+    'title': 'Neke Shoes',
     'price': 44.52,
     'imageURL': 'assets/images/shoes 4.png',
     'company': 'Nike',
@@ -33,7 +33,7 @@ final products = [
   },
   {
     'id': '6',
-    'title': 'Men\'s Nike Shoes',
+    'title': 'Gucci Shoes',
     'price': 30.52,
     'imageURL': 'assets/images/shoes 5.png',
     'company': 'Nike',
@@ -41,7 +41,7 @@ final products = [
   },
   {
     'id': '5',
-    'title': 'Men\'s Nike Shoes',
+    'title': 'Balenciaga Shoes',
     'price': 44.52,
     'imageURL': 'assets/images/shoes 6.png',
     'company': 'Nike',
@@ -49,7 +49,7 @@ final products = [
   },
   {
     'id': '6',
-    'title': 'Men\'s Nike Shoes',
+    'title': ' New balance Shoes',
     'price': 90.52,
     'imageURL': 'assets/images/shoes 4.png',
     'company': 'Nike',
